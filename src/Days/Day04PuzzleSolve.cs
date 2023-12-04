e60c6a3daca3fba34fa059b3c6575f7ef7bb5d6c
TheWebartist <thewebartist@outlook.com>
2023-12-04 21:42:54 +0100

    Complete day 4

--- src/Days/Day04PuzzleSolve.cs
+++ src/Days/Day04PuzzleSolve.cs
@@ -19,7 +19,7 @@
             => $"Winning card ? But Corpos always wins not Heywood's guys {await GetWinningCardsScoreAsync(PuzzleInputsService.INPUT_FILE_NAME)}.";
 
         public async Task<string> SolveSecondPuzzlePartAsync()
-            => $"Hey choom, be patient.";
+            => $"You want scratchcards choomba ? Ok get it : {await GetScratchCardsCountAsync(PuzzleInputsService.INPUT_FILE_NAME)}";
 
         #endregion IPuzzleSolver members
 
@@ -52,6 +52,73 @@
                 }
                 lineNumber++;
             }
+
+            return result;
+        }
+
+        private async Task<int> GetScratchCardsCountAsync(string inputFilename)
+        {
+            int result = 0;
+            Dictionary<int, int> scratchcardsDict = new Dictionary<int, int>();
+            Dictionary<int, int> copySratchcards = new Dictionary<int, int>();
+            string[] lines = await PuzzleInputsService.GetPuzzleInputLinesAsync(Day, inputFilename);
+            int lineNumber = 0;
+            foreach (string line in lines)
+            {
+                var cardParts = line.Split(PREFIX_END_SYMBOL);
+                cardParts = cardParts.Length > 1 ? cardParts[1].Split(SEPARATOR_SYMBOL) : null;
+
+                if (cardParts?.Length > 1)
+                {
+                    var winningCards = cardParts[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(v => int.TryParse(v, out var intValue) ? intValue : 0).Where(s => s > 0);
+                    var playedCards = cardParts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(v => int.TryParse(v, out var intValue) ? intValue : 0).Where(s => s > 0);
+
+                    var intersectCards = playedCards.Intersect(winningCards);
+                    var wonScratchCards = playedCards.Intersect(winningCards).Count();
+
+                    if (wonScratchCards > 0)
+                    {
+                        int loopValue = 1;
+                        if (copySratchcards.TryGetValue(lineNumber, out var copyCardsCount))
+                        {
+                            loopValue += copyCardsCount;
+                        }
+
+                        for (int i = 0; i < loopValue; i++)
+                        {
+                            for (int s = 0; s < (wonScratchCards + 1); s++)
+                            {
+                                var lineKey = lineNumber + s;
+                                if (!scratchcardsDict.ContainsKey(lineKey))
+                                {
+                                    scratchcardsDict[lineKey] = 0;
+                                }
+                                if (s > 0)
+                                {
+                                    if (!copySratchcards.ContainsKey(lineKey))
+                                    {
+                                        copySratchcards[lineKey] = 0;
+                                    }
+                                    copySratchcards[lineKey] += 1;
+                                }
+                                else
+                                {
+                                    scratchcardsDict[lineKey] += 1;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // You win original card and copies of this card even if no winning numbers in it
+                        scratchcardsDict[lineNumber] = 1 + (copySratchcards.TryGetValue(lineNumber, out var copyCardsCount) ? copyCardsCount : 0);
+                    }
+                }
+
+                lineNumber++;
+            }
+
+            result = scratchcardsDict.Values.Take(lineNumber).Sum();
 
             return result;
         }
