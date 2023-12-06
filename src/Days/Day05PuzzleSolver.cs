1c3ad62d0ad121ad5eb1c02c1b0afc451dbbf934
TheWebartist <thewebartist@outlook.com>
2023-12-06 19:50:28 +0100

    Fails on part 2 of day 5

--- src/Days/Day05PuzzleSolver.cs
+++ src/Days/Day05PuzzleSolver.cs
@@ -20,7 +20,15 @@
             => $"What's this fucking Almanach ? Obviously, that's the lowest location : {await GetLowestLocationFromAlmanachAsync(PuzzleInputsService.INPUT_FILE_NAME)}.";
 
         public async Task<string> SolveSecondPuzzlePartAsync()
-            => $"Lost in Almanach translation of part 1...";
+            => $"Hey choom, my solution isn't good and too slow with full input entry so this is the solution of the example of part 2 : {await GetLowestLocationFromMoreComplexAlmanachAsync(PuzzleInputsService.SAMPLE_PART_ONE_FILE_NAME)}";
+
+        //public async Task<string> SolveSecondPuzzlePartAsync()
+        //{
+        //    Stopwatch sw = Stopwatch.StartNew();
+        //    var result = await GetLowestLocationFromMoreComplexAlmanachAsync(PuzzleInputsService.INPUT_FILE_NAME);
+        //    sw.Stop();
+        //    return $"Hey choom, they have added some rules to seeds logix of almanach, but I've founds the result... the lowest location is {result}. result founded in {sw.Elapsed.Minutes}m {sw.Elapsed.Seconds}s and {sw.Elapsed.Milliseconds}ms.";
+        //}
 
         #endregion IPuzzleSolver members
 
@@ -28,7 +36,6 @@
 
         private async Task<long> GetLowestLocationFromAlmanachAsync(string inputFilename)
         {
-            long result = 0L;
             string[] lines = await PuzzleInputsService.GetPuzzleInputLinesAsync(Day, inputFilename);
             int lineNumber = 0;
             List<long> seeds = new List<long>();
@@ -44,6 +51,192 @@
                     if (lineParts.Length > 1)
                     {
                         seeds = lineParts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(sv => long.TryParse(sv, out var iv) ? iv : 0).ToList();
+                        lineNumber++;
+                        continue;
+                    }
+
+                    if (seeds.Count == 0)
+                    {
+                        throw new ArgumentNullException(nameof(seeds));
+                    }
+                }
+
+                if (string.IsNullOrEmpty(line))
+                {
+                    if (currentAlmanachConversionGroup != null)
+                    {
+                        almanachConversionGroups.Add(currentAlmanachConversionGroup);
+                        currentAlmanachConversionGroup = null;
+                    }
+                }
+                else
+                {
+                    var lineParts = line.Split(CONVERSION_LINE_VALUE_SEPARATOR, StringSplitOptions.RemoveEmptyEntries);
+                    if (lineParts.Length > 1 && line.IndexOf(CATEGORY_FIELD_SEPARATOR) > 0)
+                    {
+                        currentAlmanachConversionGroup = new AlmanachConversionGroup(lineParts[0]);
+                    }
+                    else
+                    {
+                        var longValues = lineParts.Select(sv => long.TryParse(sv, out var iv) ? iv : -1).Where(iv => iv > -1).ToArray();
+                        if (longValues.Length == 3 && currentAlmanachConversionGroup != null)
+                        {
+                            long destinationValue = longValues[0];
+                            long sourceValue = longValues[1];
+                            long conversionLength = longValues[2];
+
+                            currentAlmanachConversionGroup.SeedsConversionLines.Add(new ConversionLine(sourceValue, destinationValue, conversionLength));
+                        }
+                    }
+                }
+
+                lineNumber++;
+                if (lineNumber == almanachLinesCount && currentAlmanachConversionGroup != null)
+                {
+                    almanachConversionGroups.Add(currentAlmanachConversionGroup);
+                    currentAlmanachConversionGroup = null;
+                }
+            }
+
+            List<long> locationValues = new List<long>();
+
+            foreach (var seed in seeds)
+            {
+                long locationValue = seed;
+                foreach (var almanachConversionGroup in almanachConversionGroups)
+                {
+                    locationValue = almanachConversionGroup.ConvertValue(locationValue);
+                }
+                locationValues.Add(locationValue);
+            }
+            return locationValues.Min();
+        }
+
+        private async Task<long> GetLowestLocationFromMoreComplexAlmanachAsync(string inputFilename)
+        {
+            string[] lines = await PuzzleInputsService.GetPuzzleInputLinesAsync(Day, inputFilename);
+            int lineNumber = 0;
+            List<long> seeds = new List<long>();
+            List<AlmanachConversionGroup> almanachConversionGroups = new List<AlmanachConversionGroup>();
+            AlmanachConversionGroup? currentAlmanachConversionGroup = null;
+            var almanachLinesCount = lines.Length;
+
+            foreach (string line in lines)
+            {
+                if (lineNumber == 0)
+                {
+                    var lineParts = line.Split(PREFIX_SYMBOL, StringSplitOptions.RemoveEmptyEntries);
+                    if (lineParts.Length > 1)
+                    {
+                        if (lineParts.Length > 1)
+                        {
+                            seeds = lineParts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(sv => long.TryParse(sv, out var iv) ? iv : 0).ToList();
+                            lineNumber++;
+                            continue;
+                        }
+                    }
+
+                    if (seeds.Count == 0)
+                    {
+                        throw new ArgumentNullException(nameof(seeds));
+                    }
+                }
+
+                if (string.IsNullOrEmpty(line))
+                {
+                    if (currentAlmanachConversionGroup != null)
+                    {
+                        almanachConversionGroups.Add(currentAlmanachConversionGroup);
+                        currentAlmanachConversionGroup = null;
+                    }
+                }
+                else
+                {
+                    var lineParts = line.Split(CONVERSION_LINE_VALUE_SEPARATOR, StringSplitOptions.RemoveEmptyEntries);
+                    if (lineParts.Length > 1 && line.IndexOf(CATEGORY_FIELD_SEPARATOR) > 0)
+                    {
+                        currentAlmanachConversionGroup = new AlmanachConversionGroup(lineParts[0]);
+                    }
+                    else
+                    {
+                        var longValues = lineParts.Select(sv => long.TryParse(sv, out var iv) ? iv : -1).Where(iv => iv > -1).ToArray();
+                        if (longValues.Length == 3 && currentAlmanachConversionGroup != null)
+                        {
+                            long destinationValue = longValues[0];
+                            long sourceValue = longValues[1];
+                            long conversionLength = longValues[2];
+
+                            currentAlmanachConversionGroup.SeedsConversionLines.Add(new ConversionLine(sourceValue, destinationValue, conversionLength));
+                        }
+                    }
+                }
+
+                lineNumber++;
+                if (lineNumber == almanachLinesCount && currentAlmanachConversionGroup != null)
+                {
+                    almanachConversionGroups.Add(currentAlmanachConversionGroup);
+                    currentAlmanachConversionGroup = null;
+                }
+            }
+
+            List<long> locationValues = new List<long>();
+
+            var maxSeedSourceEntryValue = almanachConversionGroups.First().GetMaxSourceEntryValueForConversionGroup();
+
+            for (int i = 0; i < seeds.Count; i += 2)
+            {
+                long startSeed = seeds[i];
+                long seedsLength = seeds[i + 1];
+                long maxSeed = Math.Min(maxSeedSourceEntryValue, startSeed + seedsLength);
+
+                var testPair = new Tuple<long, long>(startSeed, startSeed + seedsLength);
+                for (long m = startSeed; m < maxSeed; m++)
+                {
+                    long locationValue = m;
+                    foreach (var almanachConversionGroup in almanachConversionGroups)
+                    {
+                        locationValue = almanachConversionGroup.ConvertValue(locationValue);
+                    }
+                    locationValues.Add(locationValue);
+                }
+
+                //foreach (var almanachConversionGroup in almanachConversionGroups)
+                //{
+                //    locationValue = almanachConversionGroup.ConvertValue(locationValue, testPair);
+                //}
+            }
+
+            return locationValues.Min();
+        }
+
+        private async Task<long> GetLowestLocationFromMoreComplexAlmanachInBadWayAsync(string inputFilename)
+        {
+            string[] lines = await PuzzleInputsService.GetPuzzleInputLinesAsync(Day, inputFilename);
+            int lineNumber = 0;
+            List<long> seeds = new List<long>();
+            List<AlmanachConversionGroup> almanachConversionGroups = new List<AlmanachConversionGroup>();
+            AlmanachConversionGroup? currentAlmanachConversionGroup = null;
+            var almanachLinesCount = lines.Length;
+
+            foreach (string line in lines)
+            {
+                if (lineNumber == 0)
+                {
+                    var lineParts = line.Split(PREFIX_SYMBOL, StringSplitOptions.RemoveEmptyEntries);
+                    if (lineParts.Length > 1)
+                    {
+                        var tempSeeds = lineParts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(sv => long.TryParse(sv, out var iv) ? iv : 0).ToList();
+                        for (int i = 0; i < tempSeeds.Count; i += 2)
+                        {
+                            long seedsLength = tempSeeds[i + 1];
+                            long startSeed = tempSeeds[i];
+                            long t = 0;
+                            while (t < seedsLength)
+                            {
+                                seeds.Add(startSeed + t);
+                                t++;
+                            }
+                        }
                         lineNumber++;
                         continue;
                     }
@@ -182,6 +375,21 @@
                 }
                 return sourceValue;
             }
+
+            public long ConvertValue(long locationValue, Tuple<long, long> testPair)
+            {
+                throw new NotImplementedException();
+            }
+
+            public long GetMinValueForConversionGroup()
+            {
+                var list = SeedsConversionLines.Select(scl => scl.DestinationValue - scl.ConversionLength).ToList();
+
+                return 0L;
+            }
+
+            public long GetMaxSourceEntryValueForConversionGroup()
+                => SeedsConversionLines.Select(scl => scl.SourceValue + scl.ConversionLength).Max();
 
             #endregion Public methods
         }
