357c346f77f76e1d8deca53f93c2e6a63c4efaea
Vincent Menant <vincent.menant@ucaya.com>
2023-12-08 00:27:32 +0100

    Fails to part 2 of day 7 (validate sample input again but not the full input)

--- src/Days/Day07PuzzleSolver.cs
+++ src/Days/Day07PuzzleSolver.cs
@@ -14,7 +14,11 @@
         /// </summary>
         private static readonly List<char> CAMEL_CARDS_TYPES = new List<char> { '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' };
 
+        private static readonly List<char> CAMEL_CARDS_TYPES_FOR_NEW_RULES = new List<char> { JOKER_CARD, '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A' };
+
         private const char LINE_VALUE_SEPARATOR = ' ';
+
+        private const char JOKER_CARD = 'J';
 
         #endregion Constants and fields
 
@@ -26,7 +30,7 @@
             => $"This camel cards game is like Poker, no ? The total winnings are : {await GetCamelCardsGameTotalWinningsAsync(PuzzleInputsService.INPUT_FILE_NAME)}.";
 
         public async Task<string> SolveSecondPuzzlePartAsync()
-            => $"Be patient choom.";
+            => $"Is the Joker is a cyberpsycho ? I thinks yes, but anyway here is the total winnings with new rules : {await GetCamelCardsGameTotalWinningsWithNewRulesAsync(PuzzleInputsService.SAMPLE_FILE_NAME)}.";
 
         #endregion IPuzzleSolver members
 
@@ -71,9 +75,49 @@
             return result;
         }
 
+        private async Task<int> GetCamelCardsGameTotalWinningsWithNewRulesAsync(string inputFilename)
+        {
+            int result = 0;
+            string[] lines = await PuzzleInputsService.GetPuzzleInputLinesAsync(Day, inputFilename);
+            List<HandPlayEntry> handsEntries = new List<HandPlayEntry>();
+            int lineNumber = 0;
+
+            foreach (string line in lines)
+            {
+                var lineParts = line.Split(LINE_VALUE_SEPARATOR, StringSplitOptions.RemoveEmptyEntries);
+
+                if (lineParts.Length > 1 && int.TryParse(lineParts[1], out var bid))
+                {
+                    var hand = lineParts[0];
+                    var handType = GetHandTypeForHandWithJokerRule(hand);
+                    handsEntries.Add(new HandPlayEntry
+                    {
+                        Bid = bid,
+                        Hand = hand,
+                        HandType = handType,
+                        IsJokerRuleApplied = true
+                    });
+                }
+
+                lineNumber++;
+            }
+
+            handsEntries = handsEntries.OrderByDescending(x => x.Rank).ToList();
+
+            int rank = handsEntries.Count;
+            foreach (var handsEntry in handsEntries)
+            {
+                Console.WriteLine($"hand {handsEntry.Hand} has {handsEntry.JokerCount} joker(s) so is a hand of type {handsEntry.HandType} (base of {GetHandTypeForHand(handsEntry.Hand)}) and  has rank n°{rank} (of {handsEntry.RankStr})");
+                result += handsEntry.Bid * rank;
+                rank--;
+            }
+
+            return result;
+        }
+
         private HandType GetHandTypeForHand(string handRepresentation)
         {
-            var cards = handRepresentation.ToCharArray().GroupBy(x => x);
+            var cards = handRepresentation.ToCharArray().GroupBy(c => c);
             var groupsCount = cards.Count();
             switch (groupsCount)
             {
@@ -93,6 +137,41 @@
                 default:
                     return HandType.HighCard;
             }
+        }
+
+        private HandType GetHandTypeForHandWithJokerRule(string handRepresentation)
+        {
+            var cards = handRepresentation.ToCharArray();
+            var jokerCount = cards.Count(c => c == JOKER_CARD);
+            var baseHandType = GetHandTypeForHand(handRepresentation);
+            switch (baseHandType)
+            {
+                case HandType.HighCard:
+                    return jokerCount > 0 ? HandType.OnePair : HandType.HighCard;
+
+                case HandType.OnePair:
+                    return jokerCount > 0 ? HandType.ThreeOfAKind : HandType.OnePair;
+
+                case HandType.TwoPairs:
+                    return jokerCount == 2 ? HandType.FourOfAKind : jokerCount == 1 ? HandType.FullHouse : HandType.TwoPairs;
+
+                case HandType.ThreeOfAKind:
+                    return jokerCount == 3 || jokerCount == 1 ? HandType.FourOfAKind : HandType.ThreeOfAKind;
+
+                case HandType.FullHouse:
+                    return (jokerCount == 2 || jokerCount == 3) ? HandType.FiveOfAKind : HandType.FullHouse;
+
+                case HandType.FourOfAKind:
+                    return jokerCount == 1 ? HandType.FiveOfAKind : HandType.FourOfAKind;
+
+                case HandType.FiveOfAKind:
+                    break;
+
+                default:
+                    break;
+            }
+
+            return baseHandType;
         }
 
         #endregion Private methods
@@ -140,10 +219,16 @@
                 get
                 {
                     var handCards = Hand.ToCharArray();
-                    var rankSeries = handCards.Select(c => CAMEL_CARDS_TYPES.IndexOf(c));
+                    var cardsValues = IsJokerRuleApplied ? CAMEL_CARDS_TYPES_FOR_NEW_RULES : CAMEL_CARDS_TYPES;
+                    var rankSeries = handCards.Select(c => 1 + cardsValues.IndexOf(c));
                     return string.Join("", rankSeries.Select(v => v.ToString("00")));
                 }
             }
+
+            public int JokerCount
+                => Hand.ToCharArray().Count(c => c == JOKER_CARD);
+
+            public bool IsJokerRuleApplied { get; set; }
 
             #endregion Properties
         }
